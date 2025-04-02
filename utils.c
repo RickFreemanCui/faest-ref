bacf171effd4b40d1b1406d7b190d3d49048d6be
Rick <freemanrickcui@outlook.com>
2025-04-02 19:02:01 +0800

    test bug

--- utils.c
+++ utils.c
@@ -61,42 +61,42 @@ bool decode_all_chall_3(uint16_t* decoded_chall, const uint8_t* chall,
 
 // RSD DecodeChall_3
 static bool rsd_decode_chall_3(uint8_t* decoded_chall, const uint8_t* chall, unsigned int i,
-  const resolved_paramset_t* params) {
-if (i >= params->tau) {
-return false;
-}
+                               const resolved_paramset_t* params) {
+  if (i >= params->tau) {
+    return false;
+  }
 
-const unsigned int t1 = params->tau1;
-const unsigned int k  = params->k;
+  const unsigned int t1 = params->tau1;
+  const unsigned int k  = params->k;
 
-unsigned int lo;
-unsigned int hi;
-if (i < t1) {
-lo = i * k;
-hi = (i + 1) * k;
-} else {
-const unsigned int t = i - t1;
+  unsigned int lo;
+  unsigned int hi;
+  if (i < t1) {
+    lo = i * k;
+    hi = (i + 1) * k;
+  } else {
+    const unsigned int t = i - t1;
 
-lo = (t1 * k) + (t * (k - 1));
-hi = (t1 * k) + ((t + 1) * (k - 1));
-}
+    lo = (t1 * k) + (t * (k - 1));
+    hi = (t1 * k) + ((t + 1) * (k - 1));
+  }
 
-assert(hi - lo == k || hi - lo == k - 1);
-// TODO: this could be implemented more efficiently using bit shifts
-for (unsigned int j = lo; j < hi; ++j) {
-ptr_set_bit(decoded_chall, j - lo, ptr_get_bit(chall, j));
-}
-return true;
+  assert(hi - lo == k || hi - lo == k - 1);
+  // TODO: this could be implemented more efficiently using bit shifts
+  for (unsigned int j = lo; j < hi; ++j) {
+    ptr_set_bit(decoded_chall, j - lo, ptr_get_bit(chall, j));
+  }
+  return true;
 }
 
 bool rsd_decode_all_chall_3(uint16_t* decoded_chall, const uint8_t* chall,
-  const resolved_paramset_t* params) {
-for (unsigned int i = 0; i != params->tau; ++i) {
-uint8_t tmp[2] = {0};
-if (!rsd_decode_chall_3(tmp, chall, i, params)) {
-return false;
-}
-decoded_chall[i] = num_rec_2(tmp);
-}
-return true;
+                            const resolved_paramset_t* params) {
+  for (unsigned int i = 0; i != params->tau; ++i) {
+    uint8_t tmp[2] = {0};
+    if (!rsd_decode_chall_3(tmp, chall, i, params)) {
+      return false;
+    }
+    decoded_chall[i] = num_rec_2(tmp);
+  }
+  return true;
 }
